92d0f0443086227d16b248175b25f658bb60ca9f
Alex Zheng <alexzheng@tamu.edu>
2023-09-30 11:10:07 -0500

    login window modification

--- src/LoginController.java
+++ src/LoginController.java
@@ -50,28 +50,51 @@ public class LoginController implements ActionListener {
         }
 
         public LoginScreen() {
-
-
             this.setSize(300, 450);
-            this.setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
+            this.setLayout(new GridBagLayout()); // Main layout
 
-            this.getContentPane().add(new JLabel ("Event Online Ticketing System"));
+            // Container JPanel to hold all elements vertically using BoxLayout
+            JPanel container = new JPanel();
+            container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
 
+            // Panel to center the title label
+            JPanel titlePanel = new JPanel(new FlowLayout());
+            titlePanel.add(new JLabel("Event Online Ticketing System"));
+            container.add(titlePanel);
+
+            // Form panel
             JPanel main = new JPanel(new SpringLayout());
             main.add(new JLabel("Username:"));
-            txtUserName.setPreferredSize(new Dimension(150, 25));
+
+            // Set fixed sizes for the username JTextField
+            Dimension size = new Dimension(150, 25);
+            txtUserName.setPreferredSize(size);
+            txtUserName.setMinimumSize(size);
+            txtUserName.setMaximumSize(size);
             main.add(txtUserName);
+
             main.add(new JLabel("Password:"));
-            txtPassword.setPreferredSize(new Dimension(150, 25));
+
+            // Set fixed sizes for the password JTextField
+            txtPassword.setPreferredSize(size);
+            txtPassword.setMinimumSize(size);
+            txtPassword.setMaximumSize(size);
             main.add(txtPassword);
 
             SpringUtilities.makeCompactGrid(main,
-                    2, 2, //rows, cols
-                    6, 6,        //initX, initY
-                    6, 6);       //xPad, yPad
+                    2, 2, // rows, cols
+                    6, 6,        // initX, initY
+                    6, 6);       // xPad, yPad
 
-            this.getContentPane().add(main);
-            this.getContentPane().add(btnLogin);
+            container.add(main);
+            container.add(btnLogin);
+
+            // Use GridBagConstraints to center the container JPanel in the window
+            GridBagConstraints gbc = new GridBagConstraints();
+            gbc.fill = GridBagConstraints.VERTICAL;
+            gbc.gridx = 0;
+            gbc.gridy = 0;
+            this.add(container, gbc);
         }
     }
 }
